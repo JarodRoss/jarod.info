b4b2e7e1b8a05b5f87806c706041fc61184021ca
JarodRoss <jarodross852@gmail.com>
2025-09-26 19:45:00 +0200

    homepage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,22 @@
+import { Helmet } from 'react-helmet-async'
+import PageTransition from '@/components/layout/PageTransition'
+import Hero from '@/components/home/Hero'
+import CurrentlyDoing from '@/components/home/CurrentlyDoing'
+import AboutPreview from '@/components/home/AboutPreview'
+import FeaturedProjects from '@/components/home/FeaturedProjects'
+import ContactCTA from '@/components/home/ContactCTA'
+
+export default function Home() {
+  return (
+    <PageTransition>
+      <Helmet>
+        <title>Jarod | Portfolio</title>
+      </Helmet>
+      <Hero />
+      <CurrentlyDoing />
+      <AboutPreview />
+      <FeaturedProjects />
+      <ContactCTA />
+    </PageTransition>
+  )
+}
