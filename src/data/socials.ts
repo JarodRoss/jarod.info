2a3e3bac2065689b7b70f459bcfda42c7a1892dd
JarodRoss <jarodross852@gmail.com>
2025-11-07 22:00:00 +0200

    fix twitch link

--- src/data/socials.ts
+++ src/data/socials.ts
@@ -2,6 +2,6 @@ import type { SocialLink } from '@/types'
 
 export const socials: SocialLink[] = [
   { name: 'GitHub', url: 'https://github.com/JarodRoss', icon: 'Github' },
-  { name: 'Twitch', url: 'https://www.twitch.tv/jarod', icon: 'Twitch' },
+  { name: 'Twitch', url: 'https://www.twitch.tv/jarod_ross', icon: 'Twitch' },
   { name: 'Email', url: 'mailto:jarodross852@gmail.com', icon: 'Mail' },
 ]
